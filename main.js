1ab2947a7f15bb27cf15dab64c19ea81dbe2b533
ImMega <72713009+ImMega@users.noreply.github.com>
2021-09-25 17:51:56 +0200

    bot init

---
+++ main.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+const { Client, Intents } = require("discord.js");
+
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
+
+client
+.on("ready", () => {
+    console.log(`${client.user.username} is now online!`);
+})
+
+client.login(process.env.TOKEN)
