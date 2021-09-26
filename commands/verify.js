f40f1e5696b6f61ba680a4e66a9443a50e41c0ab
ImMega <72713009+ImMega@users.noreply.github.com>
2021-09-26 02:15:21 +0200

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -4,26 +4,30 @@ module.exports = {
     name: "verify",
     description: "Verifies new member",
     async execute(message, args, client){
+        if(!message.mentions.members.first()) return message.reply({ content: "You need to mention a user you want to verify!", allowedMentions: { repliedUser: false } });
+
         const memberID = args[0].replace("<@!", "").replace("<@", "").replace(">", "");
         const target = message.guild.members.cache.get(memberID);
 
+        if(!target) return message.reply({ content: "Couldn't find that member!", allowedMentions: { repliedUser: false } });
+
         if(target.user.bot) return message.reply({ content: "You can't verify bots!", allowedMentions: { repliedUser: false } });
 
-        let kocho = message.member.roles.cache.some(role => role.id === `764241114575536148`);
-        let kyoto = message.member.roles.cache.some(role => role.id === `877853144334368778`);
+        let kocho = message.member.roles.cache.some(role => role.id === `864532450151628860`);
+        let kyoto = message.member.roles.cache.some(role => role.id === `864533085458268210`);
         
         if(!kocho && !kyoto) return message.reply({ content: "You can't do that!", allowedMentions: { repliedUser: false } });
 
         if(message.author.id === memberID) return message.reply({ content: "You can't verify yourself!", allowedMentions: { repliedUser: false } });
 
-        let unverifiedHas = target.roles.cache.some(role => role.id === `859820754723668028`);
+        let unverifiedHas = target.roles.cache.some(role => role.id === `864709553672683540`);
 
         if(!unverifiedHas) return message.reply({ content: "That member is already verified!", allowedMentions: { repliedUser: false } });
 
-        const unverifiedRole = message.guild.roles.cache.find(role => role.id === "859820754723668028");
-        const gakuseiRole = message.guild.roles.cache.find(role => role.id === "859820807165706270");
+        const unverifiedRole = message.guild.roles.cache.find(role => role.id === "864709553672683540");
+        const gakuseiRole = message.guild.roles.cache.find(role => role.id === "864536328937668638");
 
-        target.roles.remove(unverifiedRole)
+        await target.roles.remove(unverifiedRole)
         await target.roles.add(gakuseiRole);
 
         const clientRoleClr = message.guild.members.cache.get(client.user.id).roles.highest.hexColor;
