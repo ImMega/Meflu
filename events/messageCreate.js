fadbe30b8ec362e49ff17c40da3a260529251eea
ImMega <72713009+ImMega@users.noreply.github.com>
2021-10-03 20:43:11 +0200

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,6 +1,25 @@
+const profileModel = require("../Models/profileSchema");
+
 module.exports = {
-    run(message, client){
+    async run(message, client){
         if(!message.content.startsWith(client.prefix) || message.author.bot) return;
+
+        let profileData;
+        try {
+            profileData = await profileModel.findOne({userID: message.author.id});
+
+            if(!profileData){
+                let profile = await profileModel.create({
+                    userID: message.author.id,
+                    mainSkin: {"name" : "", "link" : ""},
+                    skins: []
+                });
+
+                profile.save();
+            }
+        } catch(err){
+            console.log(err);
+        }
 
         const args = message.content.slice(client.prefix.length).split(/ +/);
         const command = args.shift().toLowerCase();
