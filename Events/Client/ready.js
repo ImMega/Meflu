c9a1f146c26dbe2ce83c7a97f779c6f2758255a6
ImMega <72713009+ImMega@users.noreply.github.com>
2021-11-21 18:01:49 +0100

    fixes

--- Events/Client/ready.js
+++
@@ -1,7 +0,0 @@
-const { client } = require("../../main");
-
-module.exports = async () => {
-    console.log(`${client.user.username} is now online!`);
-
-    require("../../activities/statuses")();
-}
--- Events/User/userUpdate.js
+++
@@ -1,9 +0,0 @@
-const { client } = require("../../main");
-
-module.exports = (oldUser, newUser) => {
-    if(oldUser.id === "888876737914601472"){
-        client.activities.find(m => m.type === "PLAYING").name.pop();
-
-        client.activities.find(m => m.type === "PLAYING").name.push("with " + newUser.username);
-    }
-}
