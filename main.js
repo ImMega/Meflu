989662f690ea13e66131c2bbf0e5eb5869a9c2a7
ImMega <72713009+ImMega@users.noreply.github.com>
2021-11-21 17:47:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,24 +4,18 @@ const { Client, Intents } = require("discord.js");
 const mongoose = require("mongoose");
 const fs = require("fs");
 
-const client = new Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS]
-});
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
 
 module.exports = { client }
 
 client.prefix = "m!";
 client.maintenance = 0;
 
-const handlers = fs.readdirSync("./Handlers/").filter(file => file.endsWith(".js"));
-handlers.forEach(handler => require(`./Handlers/${handler}`)(client))
+fs.readdirSync("./Handlers").filter(file => file.endsWith(".js"))
+.forEach(handler => require(`./handlers/${handler}`)(client));
 
 mongoose.connect(process.env.MONGO_SRV)
-.then(() => {
-    console.log("Connected to database!");
-})
-.catch((err) => {
-    console.log(err);
-})
+.then(() => console.log("Connected to database!"))
+.catch(err => console.log(err));
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
