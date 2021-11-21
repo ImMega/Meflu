8316afcc49a813c8aade5042258b793a7819d116
ImMega <72713009+ImMega@users.noreply.github.com>
2021-11-21 17:54:50 +0100

    Replacing the command detection system

--- Events/Message/messageCreate.js
+++ Events/Message/messageCreate.js
@@ -1,36 +1,5 @@
-const profileModel = require("../../Models/profileSchema");
-const { client } = require("../../main");
+module.exports = (message) => {
+    if(message.content.startsWith("m!cmdperms") && message.author.id === "470277450551656459") return require("../../commands-permissions")(message);
 
-module.exports = async (message) => {
-    if(!message.content.startsWith(client.prefix) || message.author.bot) return require("../../actions/msgInteractions").execute(message);
-
-    if(client.maintenance === 1 && message.author.id !== "470277450551656459") return;
-
-    const args = message.content.slice(client.prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    const cmd = client.commands.get(command) || client.commands.get(client.cmdA.get(command));
-
-    if(cmd.name === "msg") return cmd.execute(message, args);
-
-    let profileData;
-    try {
-        profileData = await profileModel.findOne({userID: message.author.id});
-
-        if(!profileData){
-            let profile = await profileModel.create({
-                userID: message.author.id,
-                mainSkin: {"name" : "", "link" : ""},
-                skins: []
-            });
-
-            profile.save();
-        }
-    } catch(err){
-        console.log(err);
-    }
-
-    if(cmd){
-        cmd.execute(message, args, client);
-    }
+    require("../../activities/msgInteractions").execute(message);
 }
---
+++ events/interactions/interactionCreate.js
@@ -0,0 +1,29 @@
+const { client } = require("../../main");
+const profileModel = require("../../models/profileSchema");
+
+module.exports = (interaction) => {
+    if(client.maintenance === 1 && interaction.user.id !== "470277450551656459") return;
+
+    if(!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    let profileData;
+    try {
+        profileData = await profileModel.findOne({userID: message.author.id});
+
+        if(!profileData){
+            let profile = await profileModel.create({
+                userID: message.author.id,
+                mainSkin: {"name" : "", "link" : ""},
+                skins: []
+            });
+
+            profile.save();
+        }
+    } catch(err){
+        console.log(err);
+    }
+
+    if(command) command.execute(interaction);
+}
